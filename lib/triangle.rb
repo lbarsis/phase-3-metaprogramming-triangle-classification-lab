7745ddabdf6f1f7e4ee2f28772155e6198021894
Levi Barsis <lbarsis@gmail.com>
2022-11-29 18:09:23 -0800

    Done with assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c    
+  end
+
+  def kind
+    if (a > 0 && b > 0 && c > 0) && (a + b > c && b + c > a && c + a > b)
+      if a == b && b == c
+        :equilateral
+      elsif a==b or b==c or a==c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
